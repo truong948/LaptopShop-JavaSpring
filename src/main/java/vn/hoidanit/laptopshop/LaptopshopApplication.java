61f56a2930764f6b117b6974f1abbeb229bd2be2
Nguyen Van Truong <truong77766t@gmail.com>
2026-09-14 14:15:18 +0700

    refactor: enhance main method to print bean definitions and add MyService class

--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -2,12 +2,18 @@ package vn.hoidanit.laptopshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LaptopshopApplication.class, args);
+
+		ApplicationContext abcd = SpringApplication.run(LaptopshopApplication.class, args);
+		for (String s : abcd.getBeanDefinitionNames()) {
+			System.out.println(s);
+		}
+
 	}
 
 }
