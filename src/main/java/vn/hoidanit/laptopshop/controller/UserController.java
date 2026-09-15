8b2bd8facc5421f65970b4b21b50c51eaef0e5b5
Nguyen Van Truong <truong77766t@gmail.com>
2026-09-15 13:39:22 +0700

    refactor: update UserController to use @Controller and return static page feat: add  html for the home page response

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -3,24 +3,34 @@ package vn.hoidanit.laptopshop.controller;
 import vn.hoidanit.laptopshop.service.UserService;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-// @Controller
-// public class UserController {
-
-//   @RequestMapping("/")
-//   public String getHomePage() {
-//     return "hello";
-//   }
-// }
-
-@RestController
+@Controller
 public class UserController {
 
   private UserService userService;
 
-  @GetMapping("")
-  public String getHomePage() {
-    return "hello Java Spring MVC";
+  public UserController(UserService userService) {
+    this.userService = userService;
   }
+
+  @RequestMapping("/hoidanit")
+  public String getHomePage() {
+    String test = this.userService.handleHello();
+    return "eric.html";
+  }
+
 }
+
+// @RestController
+// public class UserController {
+// // dependency injection
+// private UserService userService;
+
+// @GetMapping("")
+// public String getHomePage() {
+// return this.userService.handleHello();
+// }
+// }
