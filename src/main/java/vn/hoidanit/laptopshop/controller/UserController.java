db095db9f56c4e41f6874dc19755ab64ea674cd3
Nguyen Van Truong <truong77766t@gmail.com>
2026-09-16 09:15:19 +0700

    feat: implement WebMvcConfig for JSP view resolution and update UserController to return JSP view

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -16,10 +16,10 @@ public class UserController {
     this.userService = userService;
   }
 
-  @RequestMapping("/hoidanit")
+  @RequestMapping("/")
   public String getHomePage() {
     String test = this.userService.handleHello();
-    return "eric.html";
+    return "hello";
   }
 
 }
