a6d199e3ecc5e590fcd6bae038e5234be2f1dd2d
Nguyen Van Truong <truong77766t@gmail.com>
2026-09-21 11:16:01 +0700

    feat: enhance domain models by adding relationships between User, Order, and Role entities

--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -1,12 +1,18 @@
 package vn.hoidanit.laptopshop.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
-
+@Table(name = "users")
 public class User {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,9 +24,12 @@ public class User {
   private String phone;
   private String avatar;
 
-  public User() {
+  @ManyToOne
+  @JoinColumn(name = "role_id")
+  private Role role;
 
-  }
+  @OneToMany(mappedBy = "user")
+  private List<Order> order;
 
   public long getId() {
     return id;
