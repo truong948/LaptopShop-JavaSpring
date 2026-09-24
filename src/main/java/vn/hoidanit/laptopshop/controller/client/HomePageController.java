55d414aa2ba874e49d3e749a8a4f239ea6fcc013
Nguyen Van Truong <truong77766t@gmail.com>
2026-09-24 15:26:07 +0700

    fix: correct homepage view path and add ItemController for product details

--- src/main/java/vn/hoidanit/laptopshop/controller/client/HomePageController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/HomePageController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomePageController {
   @GetMapping("/")
   public String getHomePage() {
-    return "client/homePage/show";
+    return "client/homepage/show";
   }
 
 }
